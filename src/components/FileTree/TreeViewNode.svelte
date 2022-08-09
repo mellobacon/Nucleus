43a914f1e8fd6fc806c99e920f70cee4f419894c
mellbacon <mellodev@outlook.com>
2022-08-09 18:07:52 -0500

    customized treeview code from carbon components

---
+++ src/components/FileTree/TreeViewNode.svelte
@@ -0,0 +1,125 @@
+<script context="module">
+  export function computeTreeLeafDepth(node) {
+    let depth = 0;
+
+    if (node == null) return depth;
+
+    let parentNode = node.parentNode;
+
+    while (parentNode != null && parentNode.getAttribute("role") !== "tree") {
+      parentNode = parentNode.parentNode;
+      if (parentNode.tagName === "LI") depth++;
+    }
+
+    return depth;
+  }
+
+  function findParentTreeNode(node) {
+    if (node.classList.contains("bx--tree-parent-node")) return node;
+    if (node.classList.contains("bx--tree")) return null;
+    return findParentTreeNode(node.parentNode);
+  }
+</script>
+
+<script>
+  /**
+   * @typedef {string | number} TreeNodeId
+   */
+
+  export let leaf = false;
+
+  /** @type {TreeNodeId} */
+  export let id = "";
+  export let name = "";
+  export let disabled = false;
+  export let path = "";
+
+  /**
+   * Specify the icon to render
+   * @type {typeof import("svelte").SvelteComponent}
+   */
+  export let icon = undefined;
+
+  import { afterUpdate, getContext } from "svelte";
+  import RenameModel from "../Modal/RenameModel.svelte";
+  import LeafNodeMenu from "./LeafNodeMenu.svelte";
+
+  let ref = null;
+  let refLabel = null;
+  let prevActiveId = undefined;
+
+  const { activeNodeId, selectedNodeIds, clickNode, selectNode, focusNode } =
+    getContext("TreeView");
+  const offset = () =>
+    computeTreeLeafDepth(refLabel) + (leaf && icon ? 2 : 2.5);
+
+  afterUpdate(() => {
+    if (id === $activeNodeId && prevActiveId !== $activeNodeId) {
+      if (!$selectedNodeIds.includes(id)) selectNode(node);
+    }
+
+    prevActiveId = $activeNodeId;
+  });
+
+  $: node = { id, name: name, expanded: false, leaf, path };
+  $: if (refLabel) {
+    refLabel.style.marginLeft = `-${offset()}rem`;
+    refLabel.style.paddingLeft = `${offset()}rem`;
+  }
+
+  let open = false;
+</script>
+
+<li
+  bind:this={ref}
+  role="treeitem"
+  {id}
+  tabindex={disabled ? undefined : -1}
+  aria-current={id === $activeNodeId || undefined}
+  aria-selected={disabled ? undefined : $selectedNodeIds.includes(id)}
+  aria-disabled={disabled}
+  class:bx--tree-node={true}
+  class:bx--tree-leaf-node={true}
+  class:bx--tree-node--active={id === $activeNodeId}
+  class:bx--tree-node--selected={$selectedNodeIds.includes(id)}
+  class:bx--tree-node--disabled={disabled}
+  class:bx--tree-node--with-icon={icon}
+  on:click|stopPropagation={() => {
+    if (disabled) return;
+    clickNode(node);
+  }}
+  on:keydown={(e) => {
+    if (
+      e.key === "ArrowLeft" ||
+      e.key === "ArrowRight" ||
+      e.key === "F2"
+    ) {
+      e.stopPropagation();
+    }
+    if (e.key === "ArrowLeft") {
+      const parentNode = findParentTreeNode(ref.parentNode);
+      if (parentNode) parentNode.focus();
+    }
+    if (e.key === "F2") {
+      e.preventDefault();
+      open = true;
+    }
+  }}
+  on:focus={() => {
+    focusNode(node);
+  }}
+>
+  <div bind:this={refLabel} class:bx--tree-node__label={true}>
+    <svelte:component this={icon} class="bx--tree-node__icon" />
+    {name}
+  </div>
+</li>
+<LeafNodeMenu target={ref} filename={name} path={path}></LeafNodeMenu>
+<RenameModel bind:open bind:filename={name} bind:path={path} />
+
+<style>
+  :global(.bx--tree-node__label) {
+    white-space: nowrap;
+    cursor: pointer;
+  }
+</style>
---
+++ src/components/FileTree/TreeViewNodeList.svelte
@@ -0,0 +1,151 @@
+<script>
+  /**
+   * @typedef {string | number} TreeNodeId
+   * @typedef {{ id: TreeNodeId; name: string; disabled?: boolean; expanded?: boolean; path?: string; }} TreeNode
+   */
+
+  /** @type {Array<TreeNode & { children?: TreeNode[] }>} */
+  export let children = [];
+  export let expanded = false;
+  export let root = false;
+
+  /** @type {string | number} */
+  export let id = "";
+  export let name = "";
+  export let disabled = false;
+  export let path = "";
+
+  /**
+   * Specify the icon to render
+   * @type {typeof import("svelte").SvelteComponent}
+   */
+  export let icon = undefined;
+
+  import { afterUpdate, getContext } from "svelte";
+  import CaretDown from "carbon-icons-svelte/lib/CaretDown.svelte";
+  import TreeViewNode, { computeTreeLeafDepth } from "./TreeViewNode.svelte";
+  import ParentNodeMenu from "./ParentNodeMenu.svelte";
+
+  let ref = null;
+  let refLabel = null;
+  let prevActiveId = undefined;
+
+  const {
+    activeNodeId,
+    selectedNodeIds,
+    expandedNodeIds,
+    clickNode,
+    selectNode,
+    expandNode,
+    focusNode,
+    toggleNode,
+  } = getContext("TreeView");
+
+  const offset = () => {
+    const depth = computeTreeLeafDepth(refLabel);
+
+    if (parent) return depth + 1;
+    if (icon) return depth + 2;
+    return depth + 2.5;
+  };
+
+  afterUpdate(() => {
+    if (id === $activeNodeId && prevActiveId !== $activeNodeId) {
+      if (!$selectedNodeIds.includes(id)) selectNode(node);
+    }
+
+    prevActiveId = $activeNodeId;
+  });
+
+  $: parent = Array.isArray(children);
+  $: node = { id, name, expanded, leaf: !parent, path };
+  $: if (refLabel) {
+    refLabel.style.marginLeft = `-${offset()}rem`;
+    refLabel.style.paddingLeft = `${offset()}rem`;
+  }
+  $: expanded = $expandedNodeIds.includes(id);
+</script>
+
+{#if root}
+  {#each children as child (child.id)}
+    {#if Array.isArray(child.children)}
+      <svelte:self {...child} />
+    {:else}
+      <TreeViewNode leaf {...child} />
+    {/if}
+  {/each}
+{:else}
+  <li
+    bind:this={ref}
+    role="treeitem"
+    {id}
+    tabindex={disabled ? undefined : -1}
+    aria-current={id === $activeNodeId || undefined}
+    aria-selected={disabled ? undefined : $selectedNodeIds.includes(id)}
+    aria-disabled={disabled}
+    class:bx--tree-node={true}
+    class:bx--tree-parent-node={true}
+    class:bx--tree-node--active={id === $activeNodeId}
+    class:bx--tree-node--selected={$selectedNodeIds.includes(id)}
+    class:bx--tree-node--disabled={disabled}
+    class:bx--tree-node--with-icon={icon}
+    aria-expanded={expanded}
+    on:keydown={(e) => {
+      if (
+        e.key === "ArrowLeft" ||
+        e.key === "ArrowRight"
+      ) {
+        e.stopPropagation();
+      }
+      if (parent && e.key === "ArrowLeft") {
+        expanded = false;
+        expandNode(node, false);
+        toggleNode(node);
+      }
+      if (parent && e.key === "ArrowRight") {
+        if (expanded) {
+          ref.lastChild.firstElementChild?.focus();
+        } else {
+          expanded = true;
+          expandNode(node, true);
+          toggleNode(node);
+        }
+      }
+    }}
+    on:focus={() => {
+      focusNode(node);
+    }}
+  >
+    <div class:bx--tree-node__label={true} bind:this={refLabel} 
+      on:click={() => {
+        if (disabled) return;
+        expanded = !expanded;
+        expandNode(node, expanded);
+        toggleNode(node);
+      }}>
+      <span class:bx--tree-parent-node__toggle={true} {disabled}>
+        <CaretDown
+          class="bx--tree-parent-node__toggle-icon {expanded &&
+            'bx--tree-parent-node__toggle-icon--expanded'}"
+        />
+      </span>
+      <span class:bx--tree-node__label__details={true}>
+        <svelte:component this={icon} class="bx--tree-node__icon" />
+        {name}
+      </span>
+    </div>
+    {#if expanded}
+      <ul role="group" class:bx--tree-node__children={true}>
+        {#each children as child (child.id)}
+          {#if Array.isArray(child.children)}
+            <svelte:self {...child} />
+          {:else}
+            <TreeViewNode leaf {...child} />
+          {/if}
+        {/each}
+      </ul>
+    {/if}
+  </li>
+{/if}
+
+<ParentNodeMenu target={[refLabel]} filename={name} path={path}></ParentNodeMenu>
