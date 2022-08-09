a2f86cef989ffe5f11bcf7cac88f8eb5dae0138d
mellbacon <mellodev@outlook.com>
2022-08-09 18:10:36 -0500

    added context menus for file tree

---
+++ src/components/FileTree/ParentNodeMenu.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+    import { ContextMenuDivider, ContextMenuGroup, ContextMenuOption } from "carbon-components-svelte";
+    import ContextMenu from "../ContextMenu/ContextMenu.svelte";
+    import CopyFile from "carbon-icons-svelte/lib/CopyFile.svelte";
+    import Paste from "carbon-icons-svelte/lib/Paste.svelte";
+    import Cut from "carbon-icons-svelte/lib/Cut.svelte";
+    import RenameModel from "../Modal/RenameModel.svelte";
+    export let target;
+    export let filename;
+    export let path;
+    let open;
+</script>
+
+<ContextMenu {target}>
+    <ContextMenuOption labelText="Edit">
+        <ContextMenuOption indented icon={CopyFile} labelText="Copy">
+            <span class="contextshortcut" slot="shortcutText">Ctrl + C</span>
+        </ContextMenuOption>
+        <ContextMenuOption indented icon={Cut} labelText="Cut">
+            <span class="contextshortcut" slot="shortcutText">Ctrl + X</span>
+        </ContextMenuOption>
+        <ContextMenuOption indented icon={Paste} labelText="Paste">
+            <span class="contextshortcut" slot="shortcutText">Ctrl + V</span>
+        </ContextMenuOption>
+        <ContextMenuOption indented labelText="Rename..." on:click={() => {open = true}}>
+            <span class="contextshortcut" slot="shortcutText">F2</span>
+        </ContextMenuOption>
+    </ContextMenuOption>
+    <ContextMenuOption labelText="Copy Path...">
+        <ContextMenuOption labelText="Copy Relative Path"></ContextMenuOption>
+        <ContextMenuOption labelText="Copy Absolute Path"></ContextMenuOption>
+    </ContextMenuOption>
+    <ContextMenuOption labelText="Show in Explorer"></ContextMenuOption>
+    <ContextMenuDivider></ContextMenuDivider>
+    <ContextMenuOption labelText="Delete...">
+        <span class="contextshortcut" slot="shortcutText">Delete</span>
+    </ContextMenuOption>
+</ContextMenu>
+
+<RenameModel bind:open bind:filename={filename} bind:path={path} />
+
+<style>
+    .contextshortcut {
+        color: #8c8c8c;
+    }
+</style>
---
+++ src/components/Modal/RenameModel.svelte
@@ -0,0 +1,122 @@
+<script lang="ts">
+    import { Modal } from "carbon-components-svelte";
+    import Input from "../Input/Input.svelte";
+    import { fs } from "@tauri-apps/api";
+    import { updateTree } from "../FileTree/TreeData";
+    export let open = false;
+    export let filename = "";
+    export let path = "/";
+    let filenameinput = "";
+    let extinput = filenameinput.split(".")[1]
+        ? filenameinput.split(".")[1]
+        : "-";
+
+    function getExt() {
+        extinput = filenameinput.split(".")[1];
+        if (extinput === undefined || extinput === "") {
+            extinput = "-";
+        }
+    }
+</script>
+
+<Modal
+    preventCloseOnClickOutside
+    bind:open
+    modalHeading="Rename {filename}"
+    on:open
+    on:close
+    passiveModal
+>
+    <div class="modelcontent">
+        <div class="description">
+            <p>Give a new name to {filename}</p>
+        </div>
+        <div class="input">
+            <Input
+                bind:value={filenameinput}
+                labelText="Name:"
+                placeholder="Enter name..."
+                on:input={() => {
+                    getExt();
+                }}
+            />
+            <div class="divider" />
+            <Input
+                readonly
+                placeholder={extinput}
+                _class="rename-ext"
+                labelText="Extension:"
+            />
+        </div>
+        <div class="buttongroup">
+            <div class="button disabled">Back</div>
+            <div
+                class="button"
+                on:click={async () => {
+                    let oldpath = path;
+                    let newpath = path.split("\\");
+                    newpath.pop();
+
+                    newpath.push(`${filenameinput}`);
+                    path = newpath.join("\\");
+
+                    await fs.renameFile(oldpath, path);
+                    await updateTree();
+                    open = false;
+                }}
+            >
+                Rename
+            </div>
+        </div>
+    </div>
+</Modal>
+
+<style>
+    .modelcontent {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+    }
+    .description {
+        width: 100%;
+    }
+    .button {
+        width: 80px;
+        height: 35px;
+        border-radius: 1px;
+        margin: 0 5px 0 5px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background-color: #2276b2;
+        color: #f9f9f9;
+        cursor: pointer;
+    }
+    .button:hover {
+        background-color: #195b8a;
+    }
+    .buttongroup {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        margin-top: 40px;
+    }
+    .disabled {
+        background-color: #a9a9a9;
+        color: #7a7a7a;
+        cursor: not-allowed;
+    }
+    .input {
+        display: flex;
+    }
+    .divider {
+        width: 0.025rem;
+        height: 2.5rem;
+        margin: 24px 0px 0 0px;
+        content: "";
+        background-color: #606060;
+    }
+    :global(.rename-ext) {
+        flex-grow: inherit;
+    }
+</style>
