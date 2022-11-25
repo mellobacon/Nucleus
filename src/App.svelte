e53600af755dad0996b074a43d848f2b84f1002d
mellobacon <mellodev@outlook.com>
2022-11-24 23:41:27 -0600

    App restructure

--- src/App.svelte
+++ src/App.svelte
@@ -5,23 +5,16 @@
 	import "carbon-components-svelte/css/g100.css";
 	import { Pane, Splitpanes } from 'svelte-splitpanes';
     import SidebarView from "./components/Sidebar/SidebarView.svelte";
-    import { showtabview, sidebartab } from "./components/Sidebar/Sidebar";
+    import { showsidebarview, tool } from "./components/Sidebar/Sidebar";
 </script>
 
 <Header />
 <div id="main">
 	<Sidebar />
 	<Splitpanes theme="editor-pane">
-		{#if $showtabview}
+		{#if $showsidebarview}
 		<Pane size={25} snapSize={5}>
-			<SidebarView>
-				<div class="sidebarheader">
-					<span>{$sidebartab.tabname}</span>
-				</div>
-				<div class="sidebarcontent">
-					<svelte:component this={$sidebartab.content}></svelte:component>
-				</div>
-			</SidebarView>
+			<SidebarView title={$tool.tabname} content={$tool.content}></SidebarView>
 		</Pane>
 		{/if}
 		<Pane>
@@ -33,23 +26,5 @@
 	#main {
 		display: flex;
 		height: calc(100% - 2rem);
-	}
-	.sidebarheader {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        width: 100%;
-        min-height: 2rem;
-        border-bottom: 1px solid #333;
-    }
-	.sidebarcontent {
-        width: 100%;
-        overflow-y: auto;
-    }
-    .sidebarcontent::-webkit-scrollbar {
-		width: 12px;
-	}
-	.sidebarcontent::-webkit-scrollbar-thumb {
-		background-color: #4c4c4c38;;
 	}
 </style>
