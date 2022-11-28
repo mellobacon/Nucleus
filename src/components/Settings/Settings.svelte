94426aa0d4c4d82cbc5f4cca6e92e493c2322c5e
mellobacon <mellodev@outlook.com>
2022-11-27 18:05:52 -0600

    Settings soon

---
+++ src/components/Settings/Settings.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+    //import SettingsList, {setting} from "../Tree/SettingsList.svelte";
+    import AppearanceSettings from "./settings/general/AppearanceSettings.svelte";
+    export let hidden = false;
+
+</script>
+<div id="settings" class:hidden>
+    <div id="settings-list">
+    </div>
+    <div id="settings-view">
+        <div id="title">Settings View</div>
+        <div id="setting">
+            <AppearanceSettings></AppearanceSettings>
+        </div>
+    </div>
+</div>
+
+<style lang="scss">
+    .hidden {
+        display: none !important;
+    }
+    #settings {
+        height: 100%;
+        width: 100%;
+        display: flex;
+    }
+    #settings-list {
+        height: 100%;
+        min-width: 9rem;
+        border-right: solid 1px #333;
+        padding-top: 1rem;
+    }
+    #settings-view {
+        height: 100%;
+        width: 100%;
+        #title {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            border-bottom: solid 1px #333;
+            font-size: 1.2rem;
+            min-height: 3rem;
+        }
+    }
+    #setting {
+        height: 100%;
+        width: 100%;
+        padding: 10px;
+    }
+</style>
---
+++ src/components/Settings/settings/general/AppearanceSettings.svelte
@@ -0,0 +1,13 @@
+<script>
+    let themes = [
+        {id: 0, name: "Light"},
+        {id: 1, name: "Dark"},
+        {id: 2, name: "Myst"}
+    ]
+    let fonts = [
+        {id: 0, name: "Roboto"},
+        {id: 1, name: "Fira Code"},
+        {id: 2, name: "Cursed"}
+    ]
+</script>
+Settings
