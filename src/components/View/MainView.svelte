6de0315a126e7e52d2d863cf81bed14f46aba960
mellobacon <mellodev@outlook.com>
2022-11-29 17:00:41 -0600

    Added notification system

--- src/components/View/MainView.svelte
+++ src/components/View/MainView.svelte
@@ -21,13 +21,26 @@
             </div>
         </Pane>
         {#if $showpanel}
-            <Pane bind:size={panelsize} minSize={4}><div class="panel">{$tool.content}</Pane>
+            <Pane bind:size={panelsize} minSize={5}><div class="panel">
+                <svelte:component this={$tool.content}></svelte:component>
+            </Pane>
         {/if}
     </Splitpanes>
     <Footer></Footer>
 </div>
 
-<style>
+<style lang="scss">
+    .panel {
+        width: 100%;
+        height: 100%;
+        overflow: overlay;
+        &::-webkit-scrollbar {
+            width: 12px;
+        }
+        &::-webkit-scrollbar-thumb {
+            background-color: #4c4c4c38;
+        }
+    }
     #container {
         height: 100%;
         overflow: hidden;
