c48589109d3ea49dfe72ddef60edeb08f6ceb2fb
mellobacon <mellodev@outlook.com>
2022-11-30 00:02:54 -0600

    aaaahhhh

--- src/components/View/Editor.svelte
+++ src/components/View/Editor.svelte
@@ -8,7 +8,7 @@
     #tabview {
         width: 100%;
         height: 100%;
-        background-color: #1C1C1C;
+        background-color: #161616;
         z-index: 1;
         position: absolute;
     }
