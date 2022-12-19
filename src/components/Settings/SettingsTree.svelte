fc8960d1400d607c8d76a51e7172564a2ca5f8a4
mellobacon <mellodev@outlook.com>
2022-12-18 19:31:01 -0600

    initial settings title value

--- src/components/Settings/SettingsTree.svelte
+++ src/components/Settings/SettingsTree.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
     import TreeView from "../FileTree/TreeView.svelte";
+</script>
+<script lang="ts" context="module">
+    import { writable } from "svelte/store";
+
     let tree = [
         {id: 0, path: "general", name: "General", children: [
             {id: 1, path: "file1", name: "Appearance"},
             {id: 2, path: "file1", name: "Shortcuts"},
         ]},
     ];
-</script>
-<script lang="ts" context="module">
-    import { writable } from "svelte/store";
-
-    export let setting = writable("");
+    export let setting = writable(`${tree[0].name}/${tree[0].children[0].name}`);
     function openSetting(event) {
         let node = event.detail;
         setting.set(`${node.parent.name}/${node.name}`);
