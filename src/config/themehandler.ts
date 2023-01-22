4f0c8b79e5f0c3dc82e1b109765888ac50fd4273
mellobacon <mellodev@outlook.com>
2023-01-22 00:48:00 -0600

    Theming code

--- src/config/themehandler.ts
+++ src/config/themehandler.ts
@@ -10,7 +10,15 @@ export function getThemes() {
     return themelist;
 }
 
-/*export async function loadTheme() {
-    let theme = await import("../config/themes/dark-theme.json");
-    return theme.theme;
-}*/
+export async function loadTheme(name: string) {
+    let themefile = await import(`../config/themes/${name}-theme.json`);
+    const theme = Object.entries(themefile.theme);
+    for (const t of theme) {
+        for (const p of Object.entries(t[1])) {
+            const property = `${t[0]}.${p[0]}`;
+            if (document.documentElement.style.getPropertyValue(property)) {
+                document.documentElement.style.setProperty(`--${property}`, p[1]);
+            }
+        }
+    }
+}
