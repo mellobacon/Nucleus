a271e3be014608c6a11d9329eb5a0227d1c51d2f
mellbacon <mellodev@outlook.com>
2023-04-26 16:10:47 -0500

    added sidebar view

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
     import Header from "./lib/Header.svelte";
-    import Sidebar from "./lib/Sidebar.svelte";
-	
+    import Sidebar, { showsidebarview, tool} from "./lib/Sidebar.svelte";
+	import { Pane, Splitpanes } from 'svelte-splitpanes';
+    import SidebarView from "./lib/SidebarView.svelte";
 </script>
 
 <Header />
 <div id="main">
 	<Sidebar />
+	<Splitpanes theme="editor-panes">
+		<Pane size={25} minSize={10} maxSize={60}></Pane>
+		<Pane></Pane>
+	</Splitpanes>
 </div>
 <style>
 	#main {
---
+++ src/lib/SidebarView.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    export let content = null;
+</script>
+<div id="sidebarview">
+    {#if content}
+        <div id="tool-view">
+            <!---
+            <svelte:component this={content}></svelte:component>
+            -->
+            {content}
+        </div>
+    {/if}
+</div>
+
+<style lang="scss">
+    #sidebarview {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+	#tool-view {
+        width: 100%;
+        overflow-y: auto;
+        &::-webkit-scrollbar {
+            width: 12px;
+        }
+        &::-webkit-scrollbar-thumb {
+            background-color: #4c4c4c38;
+        }
+    }
+</style>
