97888066773f5ff196f3a7adec69433472e0836f
mellbacon <mellodev@outlook.com>
2023-04-28 02:45:40 -0500

    added theming

--- src/config/themehandler.ts
+++ src/config/themehandler.ts
@@ -10,7 +10,20 @@ export function getThemes() {
     return themelist;
 }
 
-/*export async function loadTheme() {
-    let theme = await import("../config/themes/dark-theme.json");
-    return theme.theme;
-}*/
+export async function loadTheme(name: string) {
+    let json = await import(`../config/themes/${name.toLowerCase()}-theme.json`);
+    const theme = Object.entries(json.theme);
+    for (const entries of theme) {
+        const category = entries[0];
+        for (const property of Object.entries(entries[1])) {
+            const name = property[0];
+            const value = property[1];
+
+            const cssValue = `${category}-${name}`;
+            if (getComputedStyle(document.documentElement).getPropertyValue(`--${cssValue}`)) {
+                document.documentElement.style.setProperty(`--${cssValue}`, value);
+                //console.log(cssValue + ": " + value);
+            }
+        }
+    }
+}
