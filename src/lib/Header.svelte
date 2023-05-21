fb4bb588a76be4275d369eb9da578fbf925f55ab
mellbacon <mellodev@outlook.com>
2023-05-21 14:09:37 -0500

    feature: added Open Folder to toolbar
    Workspace name now updates on folder load

--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -3,12 +3,13 @@
     import Dropdown from "./utility/Dropdown.svelte";
     import Settings from "carbon-icons-svelte/lib/Settings.svelte";
     import { addTab, addEditorTab } from "../lib/EditorTabList.svelte";
-    import { saveFile, openFile } from "./File";
+    import { saveFile, openFile, openFolder, workspaceName } from "./File";
 
     const items = [
         {menuname: "File", children: [
             {name: "New File", shortcut: "Ctrl + N", action: () => {addEditorTab()}},
             {name: "Open File...", shortcut: "Ctrl + O", action: async () => {await openFile()}},
+            {name: "Open Folder...", shortcut: "Ctrl + K", action: async () => {await openFolder()}},
             {name: "Save File", shortcut: "Ctrl + S", action: async () => {await saveFile()}},
             {name: "Save File As...", shortcut: "Ctrl + Shift + S", action: async () => {await saveFile(true)}},
             {name: "Close Tab", disabled: true, shortcut: "", action: () => {console.warn("Feature not implemented yet.")}},
@@ -64,7 +65,7 @@
     </div>
     <div class="divider"></div>
     <div id="workspace" title="" data-tauri-drag-region>
-        Untitled Workspace
+        {$workspaceName}
     </div>
     <div id="handle" data-tauri-drag-region></div>
     <div class="tools">
