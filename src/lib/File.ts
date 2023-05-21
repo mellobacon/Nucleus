dad4e93bc1bf764bcdc19cffbaabaf4e12fc1203
mellbacon <mellodev@outlook.com>
2023-05-21 14:07:17 -0500

    feature: added ability to open folder and load directory in file tree

--- src/lib/File.ts
+++ src/lib/File.ts
@@ -1,12 +1,62 @@
 import { dialog, fs, path } from "@tauri-apps/api";
-import { get } from 'svelte/store';
+import { get, writable } from 'svelte/store';
 import { tabs, addEditorTab } from "./EditorTabList.svelte";
+import { filetree } from "./FileTree.svelte";
 
 export async function openFile() {
     let newPath = await dialog.open() as string;
     if (newPath === null) return;
     let filename = newPath.split(path.sep).pop();
     addEditorTab(newPath, filename);
+}
+
+export const workspaceName = writable("Untitled Workspace");
+export async function openFolder() {
+    let directory = await dialog.open({directory: true}) as string;
+    if (!directory) return;
+
+    let tree = await fs.readDir(directory, {recursive: true});
+    if (!tree) {
+        console.error("Cannot load directory");
+        return;
+    }
+    let directoryName = directory.split(path.sep).pop();
+    tree = [{name: directoryName, path: directory, children: buildTree(sortTree(tree))}];
+    filetree.set(tree);
+    workspaceName.set(directoryName);
+    id = 0;
+}
+
+// Need to add ids to each node for svelte to iterate over them properly
+let id = 0;
+function buildTree(tree: fs.FileEntry[]) {
+    const nodes = [];
+    for (const node of tree) {
+        const entry = {id: id, name: node.name, path: node.path, children: node.children};
+        id++;
+        if (node.children) {
+            entry.children = buildTree(sortTree(node.children));
+        }
+        nodes.push(entry);
+    }
+    return nodes;
+}
+function sortTree(tree: fs.FileEntry[]) {
+    const sortedTree = [];
+    let files = [];
+    for (const node of tree) {
+        if (node.children) {
+            sortedTree.push(node)
+            sortTree(node.children);
+        }
+        else {
+            files.push(node);
+        }
+    }
+    files.sort();
+    sortedTree.sort();
+    sortedTree.push(...files);
+    return sortedTree;
 }
 
 export async function saveFile(saveAs = false) {
