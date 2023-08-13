69b03334e270ec4c14faa9771189d7d9f7c1a8a4
mellbacon <mellodev@outlook.com>
2023-08-13 16:40:52 -0500

    feat: autodetect utf16 endianess

--- src-tauri/src/encoding.rs
+++ src-tauri/src/encoding.rs
@@ -8,22 +8,32 @@ pub enum BOM {
 pub fn convert_to_u16(str: &str, bom: BOM) -> Vec<u8> {
     let mut bytes = vec![];
 
-        if bom == BOM::BigEndian {
-            bytes = core::iter::once(0xFEFF)
-                .chain(str.encode_utf16())
+    if bom == BOM::BigEndian {
+        bytes = core::iter::once(0xFEFF)
+            .chain(str.encode_utf16())
+            .flat_map(|word| word.to_be_bytes())
+            .collect::<Vec<_>>();
+    } 
+    else if bom == BOM::LittleEndian {
+        bytes = core::iter::once(0xFFFE)
+            .chain(str.encode_utf16())
+            .flat_map(|word| word.to_le_bytes())
+            .collect::<Vec<_>>();
+    }
+    else if bom == BOM::None {
+        let utf16 = str.encode_utf16();
+
+        // autodetect endianess
+        if cfg!(target_endian = "big") {
+            bytes = utf16
                 .flat_map(|word| word.to_be_bytes())
                 .collect::<Vec<_>>();
         } 
-        else if bom == BOM::LittleEndian {
-            bytes = core::iter::once(0xFFFE)
-                .chain(str.encode_utf16())
+        else {
+            bytes = utf16
                 .flat_map(|word| word.to_le_bytes())
                 .collect::<Vec<_>>();
         }
-        else if bom == BOM::None {
-            bytes = str.encode_utf16()
-                .flat_map(|word| word.to_be_bytes())
-                .collect::<Vec<_>>();
-        }
+    }
     bytes
 }
