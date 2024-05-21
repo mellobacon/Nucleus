dc9a2a846843769e12e1cddf7bbf7e0280633a64
mellbacon <mellodev@outlook.com>
2024-05-21 13:10:06 -0500

    chore: get time function

---
+++ src/lib/Notifications/NotificationList.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { notifications } from "./notifications";
+    import Notification from "./Notification.svelte";
+</script>
+
+<div id="notifications">
+{#each $notifications as notification}
+    <Notification id={notification.id} type={notification.type} message={notification.message} read={notification.read} actions={notification.actions}></Notification>
+{/each}
+</div>
+
+<style lang="scss">
+#notifications {
+    height: calc(100% - 1.5rem);
+    padding: 5px;
+    overflow: auto;
+}
+</style>
